5c45a7184e2a192c4966f97055c319adbbd2e90a
Azharkhan3310 <muhammadzahar258@gmail.com>
2024-10-21 11:03:59 +0500

    header update

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,7 +2,7 @@ export function Header() {
     return(
         <div className="header">
             <ul className="buttons">
-                <li><a href="http://localhost:3000/"> Home </a></li>
+                <li><a href="https://giaic-q2-class-sunday-9-12-assignment-4-and-5-five.vercel.app/"> Home </a></li>
                 <li><a href="/portfolio"> Portfolio </a></li>
                 <li><a href="/about"> About </a></li>
                 <li><a href="/contact-us"> Contact Us </a></li>
